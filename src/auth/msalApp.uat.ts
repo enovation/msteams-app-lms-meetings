6813bf54fc1094d27b0ecce00edc4c3f3f24d7da
avegys <andrius.vegys@enovation.ie>
2020-06-03 17:09:37 +0300

    UAT details added

---
+++ src/auth/msalApp.uat.ts
@@ -0,0 +1,29 @@
+//shamelessly stolen from: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/samples/react-sample-app/src/auth-utils.js
+import { UserAgentApplication } from 'msal';
+
+function isIE() {
+  const ua = window.navigator.userAgent;
+  const msie = ua.indexOf('MSIE ') > -1;
+  const msie11 = ua.indexOf('Trident/') > -1;
+
+  // If you as a developer are testing using Edge InPrivate mode, please add "isEdge" to the if check
+  // const isEdge = ua.indexOf("Edge/") > -1;
+  return msie || msie11;
+}
+
+export const msalApp = new UserAgentApplication({
+  auth: {
+    clientId: '430e7481-f053-4bf0-8ad5-3fd4dbb803e3',
+    authority: 'https://login.microsoftonline.com/common',
+    validateAuthority: true,
+    postLogoutRedirectUri: 'https://enoms.enovation.ie/meetings',
+    navigateToLoginRequestUrl: false
+  },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: isIE()
+  },
+  system: {
+    navigateFrameWait: 0
+  }
+});
